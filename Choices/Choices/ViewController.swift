c7a43530b298f4e44a929d73b68f811c9633e7fa
Callista Scholer <callista.scholer@duke.edu>
2020-03-30 11:19:38 -0400

    primitive storyboard

---
+++ Choices/Choices/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Choices
+//
+//  Created by Callie Scholer on 3/30/20.
+//  Copyright © 2020 Callie Scholer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
