791bcea323fe7e9e45dd2d0e0faf926e843c51c4
Jin Cho <jin.cho098@duke.edu>
2020-04-08 00:00:11 -0400

    started SQLite3

--- Choices/Choices/ViewController.swift
+++ Choices/Choices/ViewController.swift
@@ -7,12 +7,28 @@
 //
 
 import UIKit
+import SQLite3
 
 class ViewController: UIViewController {
 
+    var db: OpaquePointer?
+    
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
+    
+    //creating a database file url to hold the data
+    let fileURL = try! FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false)
+    .appendingPathComponent("choiceLists.sqlite")
+    
+    //opening database file (error if unable to open)
+    if sqlite3_open(fileURL.path, &db) != SQLITE_OK{
+        print("Error opening database")
+    }
+    
+    //creating SQLite Table
+   
+    
   }
 
 
