6a0109cda89c065dd203e9944c938e6b71c04f95
Jin Cho <jin.cho098@duke.edu>
2020-04-12 04:59:41 -0400

    saving new list and adding new choices firebase database

---
+++ Choices/Choices/Choices.swift
@@ -0,0 +1,19 @@
+//
+//  Choices.swift
+//  Choices
+//
+//  Created by Jin Cho on 4/12/20.
+//  Copyright © 2020 Callie Scholer. All rights reserved.
+//
+
+import Foundation
+
+class Choices{
+    var id: String
+    var text: String
+    
+    init(id:String, text:String){
+        self.id = id
+        self.text = text
+    }
+}
---
+++ Choices/Choices/oldListTableViewController.swift
@@ -0,0 +1,155 @@
+//
+//  oldListTableViewController.swift
+//  Choices
+//
+//  Created by Jin Cho on 4/12/20.
+//  Copyright © 2020 Callie Scholer. All rights reserved.
+//
+
+import UIKit
+import FirebaseDatabase
+import Firebase
+
+class oldListTableViewController: UITableViewController {
+
+     var textField: UITextField!
+        var allChoices = [Choices]()
+        
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(showAddUserAlertController))
+            observeChoices()
+            // Uncomment the following line to preserve selection between presentations
+            // self.clearsSelectionOnViewWillAppear = false
+
+            // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+            // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        }
+
+        // MARK: - Table view data source
+        
+        
+        func observeChoices(){
+            let choiceRef = Database.database().reference().child("choices")
+            
+            choiceRef.observe(.value, with: {snapshot in
+                
+                var tempChoices = [Choices]()
+                
+                for child in snapshot.children{
+                    if let childSnapshot = child as? DataSnapshot,
+                        let dict = childSnapshot.value as? [String:Any],
+                        let text = dict["text"] as? String,
+                        let id = dict["id"] as? String {
+                        
+                            let item = Choices(id: id, text: text)
+                            tempChoices.append(item)
+                        }
+                    
+                }
+                
+                self.allChoices = tempChoices
+                self.tableView.reloadData()
+            }
+            )
+        }
+
+        @objc public func showAddUserAlertController() {
+            let choiceRef = Database.database().reference().child("choices").childByAutoId()
+            
+            let alertCtrl = UIAlertController(title: "Add Choice", message: "Add a new choice to the list", preferredStyle: .alert)
+
+            // Add text field to alert controller
+            alertCtrl.addTextField { (textField) in
+                self.textField = textField
+                self.textField.autocapitalizationType = .words
+                self.textField.placeholder = "e.g Hamburger"
+            }
+
+            // Add cancel button to alert controller
+            alertCtrl.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+
+            // "Add" button with callback
+            alertCtrl.addAction(UIAlertAction(title: "Add", style: .default, handler: { action in
+                if let newItem = self.textField.text, newItem != "" {
+                    let choiceObject = [
+                        "id": choiceRef.key,
+                        "text": newItem
+                    ]
+                    choiceRef.setValue(choiceObject, withCompletionBlock: { error, ref in
+                        if error == nil {
+                            self.dismiss(animated: true, completion: nil)
+                        }
+                        else{
+                            
+                        }
+                    })
+//                    self.tableView.reloadData()
+                }
+            }))
+
+            present(alertCtrl, animated: true, completion: nil)
+        }
+
+        override func numberOfSections(in tableView: UITableView) -> Int {
+            // #warning Incomplete implementation, return the number of sections
+            return 1
+        }
+
+        override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            // #warning Incomplete implementation, return the number of rows
+            return allChoices.count
+        }
+
+        
+        override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "choice", for: indexPath) as! ChoiceTableViewCell
+            cell.set(choice: allChoices[indexPath.row])
+            return cell
+        }
+        
+
+        /*
+        // Override to support conditional editing of the table view.
+        override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+            // Return false if you do not want the specified item to be editable.
+            return true
+        }
+        */
+
+        
+        // Override to support editing the table view.
+    //    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+    //        if editingStyle == .delete {
+    //            self.users.remove(at: indexPath.row)
+    //            self.tableView.deleteRows(at: [indexPath], with: .automatic)
+    //        }
+    //    }
+        
+
+        /*
+        // Override to support rearranging the table view.
+        override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+        }
+        */
+
+        /*
+        // Override to support conditional rearranging of the table view.
+        override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+            // Return false if you do not want the item to be re-orderable.
+            return true
+        }
+        */
+
+        /*
+        // MARK: - Navigation
+
+        // In a storyboard-based application, you will often want to do a little preparation before navigation
+        override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+            // Get the new view controller using segue.destination.
+            // Pass the selected object to the new view controller.
+        }
+        */
+
+}
