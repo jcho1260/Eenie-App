df658008e628320ebd77f90b221f12b7fef2f11d
Callista Scholer <callista.scholer@duke.edu>
2020-04-13 20:29:31 -0400

    BIG COMMIT

---
+++ Choices/Choices/PhotoUploadViewController.swift
@@ -0,0 +1,34 @@
+//
+//  PhotoUploadViewController.swift
+//  Choices
+//
+//  Created by Callie Scholer on 4/13/20.
+//  Copyright © 2020 Callie Scholer. All rights reserved.
+//
+
+import UIKit
+import FirebaseDatabase
+import Firebase
+
+class PhotoUploadViewController: UIViewController {
+
+  
+  
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
