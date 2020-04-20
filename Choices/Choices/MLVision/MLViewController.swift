0359a9e8e5c95ec962cc5213dbdbc8b4ce81f04f
Michelle Tai <michelle.tai@duke.edu>
2020-04-20 00:35:22 -0400

    added ml part x q q :q sdfjncskvn adcas kdskd

---
+++ Choices/Choices/MLVision/MLViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MLViewController.swift
+//  
+//
+//  Created by Michelle Tai on 4/19/20.
+//
+
+import UIKit
+
+class MLViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
