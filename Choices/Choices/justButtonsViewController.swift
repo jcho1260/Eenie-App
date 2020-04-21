dbe0b13632ae67bd174615c9fa7a2350698114e4
Callista Scholer <callista.scholer@duke.edu>
2020-04-21 15:43:41 -0400

    justButton view controller

---
+++ Choices/Choices/justButtonsViewController.swift
@@ -0,0 +1,39 @@
+//
+//  justButtonsViewController.swift
+//  Choices
+//
+//  Created by Callie Scholer on 4/21/20.
+//  Copyright © 2020 Callie Scholer. All rights reserved.
+//
+
+import UIKit
+
+class justButtonsViewController: UIViewController {
+
+  @IBOutlet weak var listButton: UIButton!
+  @IBOutlet weak var magicButton: UIButton!
+  
+    override func viewDidLoad() {
+        super.viewDidLoad()
+      
+      self.view.backgroundColor = #colorLiteral(red: 0.9764705882, green: 0.9764705882, blue: 0.9764705882, alpha: 1)
+      listButton.backgroundColor = #colorLiteral(red: 0.3450980392, green: 0.5450980392, blue: 0.5450980392, alpha: 1)
+      listButton.layer.cornerRadius = 8
+      magicButton.backgroundColor = #colorLiteral(red: 0.8705882353, green: 0.568627451, blue: 0.3176470588, alpha: 1)
+      magicButton.layer.cornerRadius = 8
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
